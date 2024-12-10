e0dfae56f64d8ec50924179cb80c66fa2015a4df
saad1551 <saad.ashraf.9094@gmail.com>
2024-12-11 02:18:48 +0500

    add breeze for login and signup

--- routes/web.php
+++ routes/web.php
@@ -3,17 +3,9 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/home', function () {
-    return view('pages.home');
-})->name('home');
-
-Route::get('/about', function () {
-    return view('pages.about');
-})->name('about');
-
-Route::get('/services', function () {
-    return view('pages.services');
-})->name('services');
+Route::get('/', function () {
+    return view('welcome');
+});
 
 Route::get('/dashboard', function () {
     return view('dashboard');
