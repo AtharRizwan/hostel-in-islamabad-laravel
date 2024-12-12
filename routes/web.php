48dac8c4514a41c1c84d0b1f22b025bfd72c7be1
Athar Rizwan <mathar.bese22seecs@seecs.edu.pk>
2024-12-12 23:37:31 +0500

    Final fixes

--- routes/web.php
+++ routes/web.php
@@ -6,37 +6,35 @@ use Illuminate\Support\Facades\Route;
 use App\Models\Service;
 use App\Models\Review;
 
-Route::get('/login', function() {
-    return "hello world";
-})->name('login');
+Route::get('/', function() {
+    // Redirect to login
+    return redirect()->route('login');
+});
 
-Route::get('/home', function () {
-    return view('pages.home');
-})->name('home');
-
-Route::get('/about', function () {
-    return view('pages.about');
-})->name('about');
-
-Route::get('/services', function () {
-    $services = Service::all();
-    $reviews = Review::all();
-    return view('pages.services', compact('services', 'reviews'));
-})->name('services');
-
-Route::post('/services', [AddReview::class, 'add'])->name('reviews.add');
-
-Route::post('/reviews/{id}', [AddReview::class, 'delete'])->name('reviews.delete');
-
-
-Route::get('/services/{service}', function (Service $service) {
-    return view('pages.service', compact('service'));
-})->name('service.show');
-
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::middleware('auth')->group(function () {
+    Route::get('/home', function () {
+        return view('pages.home');
+    })->name('home');
+    
+    Route::get('/about', function () {
+        return view('pages.about');
+    })->name('about');
+    
+    Route::get('/services', function () {
+        $services = Service::all();
+        $reviews = Review::all();
+        return view('pages.services', compact('services', 'reviews'));
+    })->name('services');
+    
+    Route::post('/services', [AddReview::class, 'add'])->name('reviews.add');
+    
+    Route::post('/reviews/{id}', [AddReview::class, 'delete'])->name('reviews.delete');
+    
+    
+    Route::get('/services/{service}', function (Service $service) {
+        return view('pages.service', compact('service'));
+    })->name('service.show');
+});
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
