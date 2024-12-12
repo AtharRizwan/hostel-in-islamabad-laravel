d112c8b20fc8b471db51a4b61dca4926cc925e85
ArsalanKhan04 <arsal4an@gmail.com>
2024-12-13 00:21:03 +0500

    Route for update service added

--- routes/web.php
+++ routes/web.php
@@ -16,25 +16,27 @@ Route::middleware('auth')->group(function () {
     Route::get('/home', function () {
         return view('pages.home');
     })->name('home');
-    
+
     Route::get('/about', function () {
         return view('pages.about');
     })->name('about');
-    
+
     Route::get('/services', function () {
         $services = Service::all();
         $reviews = Review::all();
         return view('pages.services', compact('services', 'reviews'));
     })->name('services');
-    
+
     Route::post('/services', [AddReview::class, 'add'])->name('reviews.add');
-    
+
     Route::post('/reviews/{id}', [AddReview::class, 'delete'])->name('reviews.delete');
-    
-    
+
+
     Route::get('/services/{service}', function (Service $service) {
         return view('pages.service', compact('service'));
     })->name('service.show');
+
+    Route::post('/services/{service}', [UpdateService::class, 'update'])->name('service.update');
 });
 
 Route::middleware('auth')->group(function () {
